4f69bb4c3974ed33943e15c652d1e3e3bb7ddf90
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-03 19:37:47 +0700

    CRUD REST API is DONE!

--- handler/handler.go
+++ handler/handler.go
@@ -6,8 +6,11 @@ import (
 	"laundry-app/entity"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/joho/godotenv"
 
@@ -31,75 +34,90 @@ func init() {
 	psqlInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 }
 
-func GetAllCustomer() ([]entity.Customer, error) {
+func GetAllCustomer(c *gin.Context) {
 	db := connectDb()
 	defer db.Close()
+	sorted := c.Param("sorted")
 
-	sqlStatement := "SELECT * FROM customer;"
-	rows, err := db.Query(sqlStatement)
+	query := "SELECT * FROM customer ORDER BY " + sorted + " ASC;"
+	rows, err := db.Query(query)
 	if err != nil {
-		return nil, err
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 	defer rows.Close()
 
-	customers := []entity.Customer{}
+	var customers []entity.Customer
 
 	for rows.Next() {
-		c := entity.Customer{}
-		err := rows.Scan(&c.Customer_id, &c.Name, &c.Phone, &c.Address, &c.Created_at, &c.Updated_at)
-		if err != nil {
-			return nil, err
+		var s entity.Customer
+		if err := rows.Scan(&s.Customer_id, &s.Name, &s.Phone, &s.Address, &s.Created_at, &s.Updated_at); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		customers = append(customers, c)
+		customers = append(customers, s)
 	}
 
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return customers, nil
+	c.JSON(http.StatusOK, customers)
 }
 
-func scanCustomer(rows *sql.Rows) []entity.Customer {
-	customers := []entity.Customer{}
-	var err error
-
-	for rows.Next() {
-		c := entity.Customer{}
-		err := rows.Scan(&c.Customer_id, &c.Name, &c.Phone, &c.Address, &c.Created_at, &c.Updated_at)
-		if err != nil {
-			panic(err)
-		}
-
-		customers = append(customers, c)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
-
-	return customers
-}
-
-func AddCustomer(customer entity.Customer) {
+func AddCustomer(c *gin.Context) {
 	db := connectDb()
 	defer db.Close()
-	var err error
 
-	sqlStatement := "INSERT INTO customer (customer_id, name, phone, address, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6);"
+	var s entity.Customer
+	if err := c.ShouldBindJSON(&s); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
 	rand.Seed(time.Now().UnixNano())
 	id := rand.Intn(900000) + 100000
-	customer.Customer_id = id
 
-	_, err = db.Exec(sqlStatement, id, customer.Name, customer.Phone, customer.Address, time.Now(), time.Now())
+	query := "INSERT INTO customer (customer_id, name, phone, address, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6);"
+	_, err := db.Exec(query, id, s.Name, s.Phone, s.Address, time.Now(), time.Now())
 
 	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("Succesfully Insert Data!")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
+
+	c.JSON(http.StatusCreated, s)
+}
+
+func UpdateCustomer(c *gin.Context) {
+	db := connectDb()
+	defer db.Close()
+
+	id := c.Param("id")
+	var s entity.Customer
+	if err := c.ShouldBindJSON(&s); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	query := "UPDATE customer SET name = $2, phone = $3, address = $4, updated_at = $5 WHERE customer_id = $1"
+	_, err := db.Exec(query, id, s.Name, s.Phone, s.Address, time.Now())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, s)
+}
+
+func DeleteCustomer(c *gin.Context) {
+	db := connectDb()
+	defer db.Close()
+	id := c.Param("id")
+
+	_, err := db.Exec("DELETE FROM customer WHERE customer_id = $1", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Succesfully Deleted Customer!"})
 }
 
 func connectDb() *sql.DB {
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"laundry-app/entity"
 	"laundry-app/handler"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,37 +9,10 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/customers", func(c *gin.Context) {
-		customers, err := handler.GetAllCustomer()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"data":    customers,
-			"message": "Successfully fetched customers",
-		})
-	})
-
-	r.POST("/customers", func(c *gin.Context) {
-		var newCustomer entity.Customer
-		if err := c.BindJSON(&newCustomer); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		handler.AddCustomer(newCustomer)
-
-		c.JSON(http.StatusCreated, gin.H{
-			"message": "Successfully Insert Data!",
-		})
-
-	})
+	r.GET("/customers/:sorted", handler.GetAllCustomer)
+	r.POST("/customer", handler.AddCustomer)
+	r.PUT("/customer/:id", handler.UpdateCustomer)
+	r.DELETE("/customer/:id", handler.DeleteCustomer)
 
 	r.Run(":8080")
 }
