ffeda1b764566172f79e006a30d167b41a70b29b
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-04 21:30:11 +0700

    little repair

--- main.go
+++ main.go
@@ -2,38 +2,25 @@ package main
 
 import (
 	"laundry-app/handler"
-	// "laundry-app/middleware"
+	"laundry-app/middleware"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.POST("/login", handler.Login)
 
-	// Tambahkan ini
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, // atau spesifik: "http://localhost:5500"
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Authorization", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
-
-	r.POST("/customer", handler.AddCustomer)
-	r.PUT("/customer/:id", handler.UpdateCustomer)
-	r.DELETE("/customer/:id", handler.DeleteCustomer)
-	r.GET("/customers/:sorted", handler.GetAllCustomer)
+	auth := r.Group("/")
+	auth.Use(middleware.AuthMiddleware)
+	{
+		auth.POST("/customer", handler.AddCustomer)
+		auth.PUT("/customer/:id", handler.UpdateCustomer)
+		auth.DELETE("/customer/:id", handler.DeleteCustomer)
+		auth.GET("/customers/:sorted", handler.GetAllCustomer)
+	}
 
 	port := ":" + os.Getenv("GIN_PORT")
 	r.Run(port)
 }
-
-// r.POST("/login", handler.Login)
-
-// auth := r.Group("/")
-// auth.Use(middleware.AuthMiddleware)
-// {
-
-// }
