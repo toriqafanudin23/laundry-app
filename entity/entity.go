add801800bfdf4a981e0633bf96199db6b3c4183
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-04 20:51:55 +0700

    -

--- entity/entity.go
+++ entity/entity.go
@@ -3,10 +3,10 @@ package entity
 import "time"
 
 type Customer struct {
-	Customer_id int
-	Name        string
-	Phone       string
-	Address     string
-	Created_at  time.Time
-	Updated_at  time.Time
+	Customer_id int       `json:"customer_id"`
+	Name        string    `json:"name"`
+	Phone       string    `json:"phone"`
+	Address     string    `json:"address"`
+	Created_at  time.Time `json:"created_at"`
+	Updated_at  time.Time `json:"updated_at"`
 }
--- main.go
+++ main.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"laundry-app/handler"
-	"laundry-app/middleware"
+	// "laundry-app/middleware"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/login", handler.Login)
+	// Tambahkan ini
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // atau spesifik: "http://localhost:5500"
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
-	auth := r.Group("/")
-	auth.Use(middleware.AuthMiddleware)
-	{
-		auth.GET("/customers/:sorted", handler.GetAllCustomer)
-		auth.POST("/customer", handler.AddCustomer)
-		auth.PUT("/customer/:id", handler.UpdateCustomer)
-		auth.DELETE("/customer/:id", handler.DeleteCustomer)
-	}
+	r.POST("/customer", handler.AddCustomer)
+	r.PUT("/customer/:id", handler.UpdateCustomer)
+	r.DELETE("/customer/:id", handler.DeleteCustomer)
+	r.GET("/customers/:sorted", handler.GetAllCustomer)
 
 	port := ":" + os.Getenv("GIN_PORT")
 	r.Run(port)
 }
+
+// r.POST("/login", handler.Login)
+
+// auth := r.Group("/")
+// auth.Use(middleware.AuthMiddleware)
+// {
+
+// }
